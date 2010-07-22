5631f80322934846ca9501cbd32f2917890618c1
LeafDuo <LeafDuo@008c6a83-023a-f150-cc74-46f3a10f969e>
2010-07-22 07:44:39 +0000

    Empty project, initial commit

---
+++ WordShuffleAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  WordShuffleAppDelegate.h
+//  WordShuffle
+//
+//  Created by leafduo on 7/22/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WordShuffleAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ WordShuffleAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  WordShuffleAppDelegate.m
+//  WordShuffle
+//
+//  Created by leafduo on 7/22/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "WordShuffleAppDelegate.h"
+
+@implementation WordShuffleAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
