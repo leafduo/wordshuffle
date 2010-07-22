f6687a2c03169dfac72ecbaeaa65701f9555127d
LeafDuo <LeafDuo@008c6a83-023a-f150-cc74-46f3a10f969e>
2010-07-22 13:59:36 +0000

    word picker added

---
+++ WordPicker.h
@@ -0,0 +1,19 @@
+//
+//  WordPicker.h
+//  WordShuffle
+//
+//  Created by leafduo on 7/22/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface WordPicker : NSObject {
+	BOOL test;
+	NSString *chinese, *english;
+}
+
+- (void)next;
+
+@end
---
+++ WordPicker.m
@@ -0,0 +1,26 @@
+//
+//  WordPicker.m
+//  WordShuffle
+//
+//  Created by leafduo on 7/22/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "WordPicker.h"
+
+
+@implementation WordPicker
+
+- (void)next {
+	if (test) {
+		chinese = @"测试";
+		english = @"test";
+		test = NO;
+	} else {
+		chinese = @"苹果";
+		english	= @"apple";
+		test = YES;
+	}
+}
+
+@end
