43f648ebe692c69f787af77729838aa559b0a465
LeafDuo <LeafDuo@008c6a83-023a-f150-cc74-46f3a10f969e>
2010-07-22 08:42:25 +0000

    A sample implement

---
+++ WordController.h
@@ -0,0 +1,15 @@
+//
+//  WordController.h
+//
+//  Created by leafduo on 7/22/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WordController : NSObject {
+    IBOutlet NSTextField* Chinese;
+    IBOutlet NSTextField* English;
+}
+- (IBAction)Change:(id)sender;
+@end
