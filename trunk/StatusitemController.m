b9ac5337b7627fd6d0470dbae373e4e20e4b869d
LeafDuo <LeafDuo@008c6a83-023a-f150-cc74-46f3a10f969e>
2010-07-28 10:02:21 +0000

    a test menu added

--- trunk/StatusitemController.m
+++ trunk/StatusitemController.m
@@ -12,9 +12,14 @@
 @implementation StatusitemController
 
 - (void)awakeFromNib {
-	item = [[NSStatusBar systemStatusBar]statusItemWithLength:NSVariableStatusItemLength];
+	item = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
 	[item retain];
 	[item setTitle:@"WS"];
+	menu = [[NSMenu alloc] initWithTitle:@"Menu"];
+	NSMenuItem *menuItem = [[NSMenuItem alloc] initWithTitle:@"test" action:NULL keyEquivalent:@""];
+	[menuItem retain];
+	[menu addItem:menuItem];
+	[item setMenu:menu];
 }
 
 @end
