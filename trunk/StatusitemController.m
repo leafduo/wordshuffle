bd82c9cb71c07ca2df8c2f872c028e4b6a7c23d6
LeafDuo <LeafDuo@008c6a83-023a-f150-cc74-46f3a10f969e>
2010-07-28 16:10:37 +0000

    exit method changed

--- trunk/StatusitemController.m
+++ trunk/StatusitemController.m
@@ -17,10 +17,15 @@
 	[item setTitle:@"WS"];
 	menu = [[NSMenu alloc] initWithTitle:@"Menu"];
 	[[menu addItemWithTitle:@"Hide/Reopen" action:@selector(hideOrReopen:) keyEquivalent:@""] setTarget:self];
-	[[menu addItemWithTitle:@"Preferences..." action:NULL keyEquivalent:@""] setTarget:nil];
+	[[menu addItemWithTitle:@"Preferences..." action:@selector(selectFile:) keyEquivalent:@""] setTarget:self];
+	[[menu addItemWithTitle:@"Select Word File" action:NULL keyEquivalent:@""] setTarget:nil];
     [menu addItem: [NSMenuItem separatorItem]];
 	[[menu addItemWithTitle:@"Quit" action:@selector(quit:) keyEquivalent:@""] setTarget:self];
 	[item setMenu:menu];
+}
+
+- (void)selectFile:(id)sender {
+	
 }
 
 - (void)hideOrReopen:(id)sender {
@@ -32,7 +37,7 @@
 }
 
 - (void)quit:(id)sender {
-	exit(0);
+	[NSApp terminate:self];
 }
 
 @end
