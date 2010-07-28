dc819803c4fa3804dbe70464afebe70f20fa8fcf
LeafDuo <LeafDuo@008c6a83-023a-f150-cc74-46f3a10f969e>
2010-07-28 10:55:50 +0000

    Implement quit

--- StatusitemController.m
+++ StatusitemController.m
@@ -17,11 +17,16 @@
 	[item setTitle:@"WS"];
 	menu = [[NSMenu alloc] initWithTitle:@"Menu"];
 	[[menu addItemWithTitle:@"Reopen Window" action:@selector(reopenWindow:) keyEquivalent:@""] setTarget:self];
+	[[menu addItemWithTitle:@"Quit" action:@selector(quit:) keyEquivalent:@""] setTarget:self];
 	[item setMenu:menu];
 }
 
 - (void)reopenWindow:(id)sender {
 	[mainWindow orderFront:self];
+}
+
+- (void)quit:(id)sender {
+	exit(0);
 }
 
 @end
