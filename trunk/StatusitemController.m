2acb97e8daae8f938484324acd56b14596312540
LeafDuo <LeafDuo@008c6a83-023a-f150-cc74-46f3a10f969e>
2010-07-31 14:35:41 +0000

    addAppAsLoginItem

--- trunk/StatusitemController.m
+++ trunk/StatusitemController.m
@@ -11,7 +11,66 @@
 
 @implementation StatusitemController
 
+- (void)addAppAsLoginItem {
+	NSString * appPath = [[NSBundle mainBundle] bundlePath];
+	
+	// This will retrieve the path for the application
+	// For example, /Applications/test.app
+	CFURLRef url = (CFURLRef)[NSURL fileURLWithPath:appPath]; 
+	
+	// Create a reference to the shared file list.
+	// We are adding it to the current user only.
+	// If we want to add it all users, use
+	// kLSSharedFileListGlobalLoginItems instead of
+	//kLSSharedFileListSessionLoginItems
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
+															kLSSharedFileListSessionLoginItems, NULL);
+	if (loginItems) {
+		//Insert an item to the list.
+		LSSharedFileListItemRef listItem = LSSharedFileListInsertItemURL(loginItems,
+																	 kLSSharedFileListItemLast, NULL, NULL,
+																	 url, NULL, NULL);
+		if (listItem){
+			CFRelease(listItem);
+		}
+	}	
+	
+	CFRelease(loginItems);
+}
+
+- (void)deleteAppFromLoginItem {
+	NSString * appPath = [[NSBundle mainBundle] bundlePath];
+	
+	// This will retrieve the path for the application
+	// For example, /Applications/test.app
+	CFURLRef url = (CFURLRef)[NSURL fileURLWithPath:appPath]; 
+	
+	// Create a reference to the shared file list.
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
+															kLSSharedFileListSessionLoginItems, NULL);
+	
+	if (loginItems) {
+		UInt32 seedValue;
+		//Retrieve the list of Login Items and cast them to
+		// a NSArray so that it will be easier to iterate.
+		NSArray  *loginItemsArray = (NSArray *)LSSharedFileListCopySnapshot(loginItems, &seedValue);
+		for(int i = 0 ; i < [loginItemsArray count]; i++){
+			LSSharedFileListItemRef itemRef = (LSSharedFileListItemRef)[loginItemsArray
+																		objectAtIndex:i];
+			//Resolve the item with URL
+			if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*) &url, NULL) == noErr) {
+				NSString * urlPath = [(NSURL*)url path];
+				if ([urlPath compare:appPath] == NSOrderedSame){
+					LSSharedFileListItemRemove(loginItems,itemRef);
+				}
+			}
+		}
+		[loginItemsArray release];
+	}
+}
+
 - (void)awakeFromNib {
+	[self addAppAsLoginItem];
 	item = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
 	[item retain];
 	NSBundle *bundle = [NSBundle mainBundle];
