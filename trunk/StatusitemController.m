4bc334251f769e90a1b12b72f35dcd71860a9c9b
LeafDuo <LeafDuo@008c6a83-023a-f150-cc74-46f3a10f969e>
2010-08-04 15:26:51 +0000

    __MyCompanyName__ -> leafduo.com

--- trunk/StatusitemController.m
+++ trunk/StatusitemController.m
@@ -3,7 +3,7 @@
 //  WordShuffle
 //
 //  Created by leafduo on 7/28/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  Copyright 2010 leafduo.com. All rights reserved.
 //
 
 #import "StatusitemController.h"
