87b118f81573da42b6deaa038dd9ca9bb33a0abb
LeafDuo <LeafDuo@008c6a83-023a-f150-cc74-46f3a10f969e>
2010-08-04 13:55:00 +0000

    startup at login

--- trunk/PreferenceController.m
+++ trunk/PreferenceController.m
@@ -11,12 +11,105 @@
 
 @implementation PreferenceController
 
-- (BOOL)isStartupAtLogin {
-	return YES;
+- (void)addAppAsLoginItem {
+	NSString * appPath = [[NSBundle mainBundle] bundlePath];
+	
+	// This will retrieve the path for the application
+	// For example, /Applications/test.app
+	CFURLRef url = (CFURLRef)[NSURL fileURLWithPath:appPath]; 
+	
+	// Create a reference to the shared file list.
+	// We are adding it to the current user only.
+	// If we want to add it all users, use
+	// kLSSharedFileListGlobalLoginItems instead of
+	//kLSSharedFileListSessionLoginItems
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
+															kLSSharedFileListSessionLoginItems, NULL);
+	if (loginItems) {
+		//Insert an item to the list.
+		LSSharedFileListItemRef listItem = LSSharedFileListInsertItemURL(loginItems,
+																		 kLSSharedFileListItemLast, NULL, NULL,
+																		 url, NULL, NULL);
+		if (listItem){
+			CFRelease(listItem);
+		}
+	}	
+	
+	CFRelease(loginItems);
 }
 
-- (IBAction)startUpAtLoginChanged:(id)sender {
+- (void)deleteAppFromLoginItem {
+	NSString * appPath = [[NSBundle mainBundle] bundlePath];
+	
+	// This will retrieve the path for the application
+	// For example, /Applications/test.app
+	CFURLRef url = (CFURLRef)[NSURL fileURLWithPath:appPath]; 
+	
+	// Create a reference to the shared file list.
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
+															kLSSharedFileListSessionLoginItems, NULL);
+	
+	if (loginItems) {
+		UInt32 seedValue;
+		//Retrieve the list of Login Items and cast them to
+		// a NSArray so that it will be easier to iterate.
+		NSArray  *loginItemsArray = (NSArray *)LSSharedFileListCopySnapshot(loginItems, &seedValue);
+		for(int i = 0 ; i < [loginItemsArray count]; i++){
+			LSSharedFileListItemRef itemRef = (LSSharedFileListItemRef)[loginItemsArray
+																		objectAtIndex:i];
+			//Resolve the item with URL
+			if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*) &url, NULL) == noErr) {
+				NSString * urlPath = [(NSURL*)url path];
+				if ([urlPath compare:appPath] == NSOrderedSame){
+					LSSharedFileListItemRemove(loginItems,itemRef);
+				}
+			}
+		}
+		[loginItemsArray release];
+	}
+}
+
+- (BOOL)isStartupAtLogin {
+	BOOL result = NO;
+	NSString * appPath = [[NSBundle mainBundle] bundlePath];
+	
+	// This will retrieve the path for the application
+	// For example, /Applications/test.app
+	CFURLRef url = (CFURLRef)[NSURL fileURLWithPath:appPath]; 
+	
+	// Create a reference to the shared file list.
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
+															kLSSharedFileListSessionLoginItems, NULL);
+	
+	if (loginItems) {
+		UInt32 seedValue;
+		//Retrieve the list of Login Items and cast them to
+		// a NSArray so that it will be easier to iterate.
+		NSArray  *loginItemsArray = (NSArray *)LSSharedFileListCopySnapshot(loginItems, &seedValue);
+		for(int i = 0 ; i < [loginItemsArray count]; i++){
+			LSSharedFileListItemRef itemRef = (LSSharedFileListItemRef)[loginItemsArray
+																		objectAtIndex:i];
+			//Resolve the item with URL
+			if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*) &url, NULL) == noErr) {
+				NSString * urlPath = [(NSURL*)url path];
+				if ([urlPath compare:appPath] == NSOrderedSame){
+					result = YES;
+				}
+			}
+		}
+		[loginItemsArray release];
+	}
+	return result;
+}
+
+- (IBAction)startUpAtLoginChanged:(NSButton*)sender {
 	NSLog(@"startUpAtLoginChanged called");
+	if ([sender state]) {
+		[self addAppAsLoginItem];
+	} else {
+		[self deleteAppFromLoginItem];
+	}
+
 }
 
 - (IBAction)displayPreferenceWindow:(id)sender {
@@ -28,7 +121,11 @@
 	NSLog(@"preferenceWindowController: %@", preferenceWindowController);
 	NSLog(@"preferenceWindow: %@", preferenceWindow);
 	[preferenceWindowController showWindow:self];
-	[preferenceWindow orderFront:self];
+	if ([self isStartupAtLogin]) {
+		[startUpAtLogin setState:NSOnState];
+	} else {
+		[startUpAtLogin setState:NSOffState];
+	}
 }
 
 @end
