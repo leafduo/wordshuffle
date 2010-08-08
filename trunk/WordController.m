55b347be1550a1199796fada6cee90489e8ff2a3
LeafDuo <LeafDuo@008c6a83-023a-f150-cc74-46f3a10f969e>
2010-08-08 12:57:00 +0000

    display time notification works but need FIX

--- trunk/WordController.m
+++ trunk/WordController.m
@@ -24,7 +24,32 @@
 
 - (void)dealloc {
 	[picker release];
+	[[NSUserDefaultsController sharedUserDefaultsController]removeObserver:self forKeyPath:@"values.englishWordDisplayTime"];
+	[[NSUserDefaultsController sharedUserDefaultsController]removeObserver:self forKeyPath:@"values.translationDisplayTime"];
 	[super dealloc];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+	//FIXME: should use [change objectForKey:]
+	
+	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	if ([keyPath isEqualToString:@"values.englishWordDisplayTime"]) {
+		NSLog(@"%@", [change description]);
+		englishWordDisplayTime = [defaults doubleForKey:@"englishWordDisplayTime"];
+		//englishWordDisplayTime = [change objectForKey:NSKeyValueChangeNewKey]doubleValue];
+		if (englishWordDisplayTime < 1.e-5) {
+			englishWordDisplayTime = 3;
+			[defaults setDouble:3 forKey:@"englishWordDisplayTime"];
+		}
+	} else if ([keyPath isEqualToString:@"values.translationDisplayTime"]) {
+		NSLog(@"%@", [change description]);
+		translationDisplayTime = [defaults doubleForKey:@"translationDisplayTime"];
+		//translationDisplayTime = [change objectForKey:NSKeyValueChangeNewKey];
+		if (translationDisplayTime < 1e-5) {
+			translationDisplayTime = 2;
+			[defaults setDouble:2 forKey:@"translationDisplayTime"];
+		}
+	}
 }
 
 - (void)awakeFromNib {
@@ -39,6 +64,8 @@
 		translationDisplayTime = 2;
 		[defaults setDouble:2 forKey:@"translationDisplayTime"];
 	}
+	[[NSUserDefaultsController sharedUserDefaultsController]addObserver:self forKeyPath:@"values.englishWordDisplayTime" options:NSKeyValueObservingOptionNew context:NULL];
+	[[NSUserDefaultsController sharedUserDefaultsController]addObserver:self forKeyPath:@"values.translationDisplayTime" options:NSKeyValueObservingOptionNew context:NULL];
 	picker = [[WordPicker alloc] init];
 	if (!picker.isFileOpen) {
 		[statusitem selectFile:self];
